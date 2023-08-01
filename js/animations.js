36bac7193d2ed56c4dfe039aea369d0be7d7464e
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-08-01 10:39:50 +0200

    feat: two-columns section + scrolltriger + onload animations

--- js/animations.js
+++ js/animations.js
@@ -1,16 +1,17 @@
-var tl = gsap.timeline();
+var heroTL = gsap.timeline({ paused: true });
 
-tl.fromTo(
-	'.hero__line',
-	{
-		width: '0'
-	},
-	{
-		width: '80%',
-		duration: 1,
-		ease: 'strong.inOut'
-	}
-)
+heroTL
+	.fromTo(
+		'.hero__line',
+		{
+			width: '0'
+		},
+		{
+			width: '80%',
+			duration: 1,
+			ease: 'strong.inOut'
+		}
+	)
 	.fromTo(
 		'.hero__img',
 		{
@@ -34,3 +35,67 @@ tl.fromTo(
 			ease: 'strong.inOut'
 		}
 	);
+
+var twoColumnsTL = gsap.timeline({ paused: true });
+
+twoColumnsTL
+	.fromTo(
+		'.two-columns__left',
+		{
+			x: '-130%'
+		},
+		{
+			x: 0,
+			duration: 1,
+			ease: 'strong.inOut'
+		}
+	)
+	.fromTo(
+		'.two-columns__right',
+		{
+			x: '130%'
+		},
+		{
+			x: 0,
+			duration: 1,
+			ease: 'strong.inOut'
+		},
+		'>-0.5'
+	)
+	.fromTo(
+		'.two-columns__left__content',
+		{
+			height: '0'
+		},
+		{
+			height: 'auto',
+			duration: 1,
+			ease: 'strong.inOut'
+		},
+		'>-0.5'
+	)
+	.fromTo(
+		'.two-columns__right__content',
+		{
+			height: '0'
+		},
+		{
+			height: 'auto',
+			duration: 1,
+			ease: 'strong.inOut'
+		},
+		'>-0.5'
+	);
+
+ScrollTrigger.create({
+	trigger: '.two-columns',
+	start: 'top 60%',
+	id: 'two-columns-scroll-trigger',
+	end: 'bottom bottom',
+	animation: twoColumnsTL,
+	toggleActions: 'play none none forward'
+});
+
+window.addEventListener('load', function () {
+	heroTL.play();
+});
