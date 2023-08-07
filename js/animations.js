b3793e9d1c856e02d4e1115e26a099e583456b86
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-08-07 15:29:49 +0200

    feat: update animations

--- js/animations.js
+++ js/animations.js
@@ -1,158 +1,291 @@
-var heroTL = gsap.timeline({ paused: true });
-heroTL
-	.fromTo(
-		'.hero__line',
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// START HERO ANIMATIONS
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+var heroElements = document.getElementsByClassName('hero');
+
+for (var i = 0; i < heroElements.length; i++) {
+	var heroTL = gsap.timeline({ paused: true });
+	heroTL
+		.fromTo(
+			heroElements[i].getElementsByClassName('hero__line')[0],
+			{
+				width: '0'
+			},
+			{
+				width: '80%',
+				duration: 1,
+				ease: 'strong.inOut'
+			}
+		)
+		.fromTo(
+			heroElements[i].getElementsByClassName('hero__img')[0],
+			{
+				width: '0'
+			},
+			{
+				width: '90%',
+				duration: 1,
+				ease: 'strong.inOut'
+			},
+			'>-1'
+		)
+		.fromTo(
+			heroElements[i].getElementsByClassName('hero__content')[0],
+			{
+				height: '0'
+			},
+			{
+				height: 'auto',
+				duration: 1,
+				ease: 'strong.inOut'
+			}
+		);
+
+	ScrollTrigger.create({
+		trigger: heroElements[i],
+		start: 'top 60%',
+		id: 'hero-scroll-trigger',
+		end: 'bottom bottom',
+		animation: heroTL,
+		toggleActions: 'play none none forward'
+	});
+}
+
+var heroFlatElements = document.getElementsByClassName('hero--flat');
+
+for (var i = 0; i < heroFlatElements.length; i++) {
+	var heroFlatTL = gsap.timeline({ paused: true });
+	heroFlatTL
+		.fromTo(
+			heroFlatElements[i].getElementsByClassName('hero--flat__line')[0],
+			{
+				width: '0'
+			},
+			{
+				width: '100%',
+				duration: 1,
+				ease: 'strong.inOut'
+			}
+		)
+		.fromTo(
+			heroFlatElements[i].getElementsByClassName('hero--flat__img')[0],
+			{
+				width: '0'
+			},
+			{
+				width: '100%',
+				duration: 1,
+				ease: 'strong.inOut'
+			},
+			'>-1'
+		)
+		.fromTo(
+			heroFlatElements[i].getElementsByClassName(
+				'hero--flat__content'
+			)[0],
+			{
+				height: '0'
+			},
+			{
+				height: 'auto',
+				duration: 1,
+				ease: 'strong.inOut'
+			}
+		);
+
+	ScrollTrigger.create({
+		trigger: heroFlatElements[i],
+		start: 'top 60%',
+		id: 'hero-flat-scroll-trigger',
+		end: 'bottom bottom',
+		animation: heroFlatTL,
+		toggleActions: 'play none none forward'
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// END HERO ANIMATIONS
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// START TWO COLUMNS
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+var twoColumnsElements = document.getElementsByClassName('two-columns');
+
+for (var i = 0; i < twoColumnsElements.length; i++) {
+	var twoColumnsTL = gsap.timeline({ paused: true });
+	twoColumnsTL
+		.fromTo(
+			twoColumnsElements[i].getElementsByClassName(
+				'two-columns__left'
+			)[0],
+			{
+				x: '-130%'
+			},
+			{
+				x: 0,
+				duration: 1,
+				ease: 'strong.inOut'
+			}
+		)
+		.fromTo(
+			twoColumnsElements[i].getElementsByClassName(
+				'two-columns__right'
+			)[0],
+			{
+				x: '130%'
+			},
+			{
+				x: 0,
+				duration: 1,
+				ease: 'strong.inOut'
+			},
+			'>-0.5'
+		);
+
+	ScrollTrigger.create({
+		trigger: twoColumnsElements[i],
+		start: 'top 60%',
+		id: 'two-columns-scroll-trigger',
+		end: 'bottom bottom',
+		animation: twoColumnsTL,
+		toggleActions: 'play none none forward'
+	});
+}
+
+var twoColumnsFlatElements =
+	document.getElementsByClassName('two-columns--flat');
+
+for (var i = 0; i < twoColumnsFlatElements.length; i++) {
+	var twoColumnsFlatTL = gsap.timeline({ paused: true });
+	twoColumnsFlatTL
+		.fromTo(
+			twoColumnsFlatElements[i].getElementsByClassName(
+				'two-columns--flat__left'
+			)[0],
+			{
+				y: '10%',
+				opacity: 0
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 1
+			}
+		)
+		.fromTo(
+			twoColumnsFlatElements[i].getElementsByClassName(
+				'two-columns--flat__right'
+			)[0],
+			{
+				y: '10%',
+				opacity: 0
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 1
+			},
+			'>-0.5'
+		);
+
+	ScrollTrigger.create({
+		trigger: twoColumnsFlatElements[i],
+		start: 'top 60%',
+		id: 'two-columns-flat-scroll-trigger',
+		end: 'bottom bottom',
+		animation: twoColumnsFlatTL,
+		toggleActions: 'play none none forward'
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// END TWO COLUMNS
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// START COLLECTIONS GRID
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+var collectionsGridElements =
+	document.getElementsByClassName('collections-grid');
+
+for (var i = 0; i < collectionsGridElements.length; i++) {
+	var items = collectionsGridElements[i].getElementsByClassName(
+		'collections-grid__item'
+	);
+
+	var CGTL = gsap.timeline({ paused: true });
+
+	CGTL.fromTo(
+		collectionsGridElements[i].getElementsByClassName(
+			'collections-grid__title'
+		)[0],
 		{
-			width: '0'
+			y: '100%',
+			opacity: 0
 		},
 		{
-			width: '80%',
-			duration: 1,
-			ease: 'strong.inOut'
-		}
-	)
-	.fromTo(
-		'.hero__img',
-		{
-			width: '0'
-		},
-		{
-			width: '90%',
-			duration: 1,
-			ease: 'strong.inOut'
-		},
-		'>-1'
-	)
-	.fromTo(
-		'.hero__content',
-		{
-			height: '0'
-		},
-		{
-			height: 'auto',
-			duration: 1,
-			ease: 'strong.inOut'
+			y: '0',
+			opacity: 1,
+			duration: 0.5
 		}
 	);
 
-ScrollTrigger.create({
-	trigger: '.hero',
-	start: 'top 60%',
-	id: 'hero-scroll-trigger',
-	end: 'bottom bottom',
-	animation: heroTL,
-	toggleActions: 'play none none forward'
-});
+	for (var j = 0; j < items.length; j++) {
+		CGTL.fromTo(
+			items[j],
+			{
+				y: '10%',
+				opacity: 0
+			},
+			{
+				y: '0',
+				opacity: 1,
+				duration: 0.4
+			}
+		);
+	}
 
-var heroFlatTL = gsap.timeline({ paused: true });
-heroFlatTL
-	.fromTo(
-		'.hero--flat__line',
-		{
-			width: '0'
-		},
-		{
-			width: '100%',
-			duration: 1,
-			ease: 'strong.inOut'
-		}
-	)
-	.fromTo(
-		'.hero--flat__img',
-		{
-			width: '0'
-		},
-		{
-			width: '100%',
-			duration: 1,
-			ease: 'strong.inOut'
-		},
-		'>-1'
-	)
-	.fromTo(
-		'.hero--flat__content',
-		{
-			height: '0'
-		},
-		{
-			height: 'auto',
-			duration: 1,
-			ease: 'strong.inOut'
-		}
-	);
+	ScrollTrigger.create({
+		trigger: collectionsGridElements[i],
+		start: 'top 60%',
+		id: 'collections-grid-scroll-trigger',
+		end: 'bottom bottom',
+		animation: CGTL,
+		toggleActions: 'play none none forward'
+	});
+}
 
-ScrollTrigger.create({
-	trigger: '.hero--flat',
-	start: 'top 60%',
-	id: 'hero-flat-scroll-trigger',
-	end: 'bottom bottom',
-	animation: heroFlatTL,
-	toggleActions: 'play none none forward'
-});
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// END COLLECTIONS GRID
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
-var twoColumnsTL = gsap.timeline({ paused: true });
-twoColumnsTL
-	.fromTo(
-		'.two-columns__left',
-		{
-			x: '-130%'
-		},
-		{
-			x: 0,
-			duration: 1,
-			ease: 'strong.inOut'
-		}
-	)
-	.fromTo(
-		'.two-columns__right',
-		{
-			x: '130%'
-		},
-		{
-			x: 0,
-			duration: 1,
-			ease: 'strong.inOut'
-		},
-		'>-0.5'
-	)
-	.fromTo(
-		'.two-columns__left__content',
-		{
-			height: '0'
-		},
-		{
-			height: 'auto',
-			duration: 1,
-			ease: 'strong.inOut'
-		},
-		'>-0.5'
-	)
-	.fromTo(
-		'.two-columns__right__content',
-		{
-			height: '0'
-		},
-		{
-			height: 'auto',
-			duration: 1,
-			ease: 'strong.inOut'
-		},
-		'>-1'
-	);
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// START E-MAIL SUB
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
-ScrollTrigger.create({
-	trigger: '.two-columns',
-	start: 'top 60%',
-	id: 'two-columns-scroll-trigger',
-	end: 'bottom bottom',
-	animation: twoColumnsTL,
-	toggleActions: 'play none none forward'
-});
+var eMailSubElements = document.getElementsByClassName('email-sub');
 
-var twoColumnsFlatTL = gsap.timeline({ paused: true });
-twoColumnsFlatTL
-	.fromTo(
-		'.two-columns--flat__left',
+for (var i = 0; i < eMailSubElements.length; i++) {
+	var eMailSubTL = gsap.timeline({ paused: true });
+	eMailSubTL.fromTo(
+		eMailSubElements[i],
 		{
 			y: '10%',
 			opacity: 0
@@ -160,30 +293,22 @@ twoColumnsFlatTL
 		{
 			y: 0,
 			opacity: 1,
-			duration: 1,
-			ease: 'strong.inOut'
+			duration: 1
 		}
-	)
-	.fromTo(
-		'.two-columns--flat__right',
-		{
-			y: '10%',
-			opacity: 0
-		},
-		{
-			y: 0,
-			opacity: 1,
-			duration: 1,
-			ease: 'strong.inOut'
-		},
-		'>-1'
 	);
 
-ScrollTrigger.create({
-	trigger: '.two-columns--flat',
-	start: 'top 60%',
-	id: 'two-columns-flat-scroll-trigger',
-	end: 'bottom bottom',
-	animation: twoColumnsFlatTL,
-	toggleActions: 'play none none forward'
-});
+	ScrollTrigger.create({
+		trigger: eMailSubElements[i],
+		start: 'top 60%',
+		id: 'two-columns-flat-scroll-trigger',
+		end: 'bottom bottom',
+		animation: eMailSubTL,
+		toggleActions: 'play none none forward'
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// END E-MAIL SUB
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
