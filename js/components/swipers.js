43d09699dc64f79c327404904d3480b6d8b75341
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-09-01 17:31:34 +0200

    feat: featured-slide + update: utilities

--- js/components/swipers.js
+++ js/components/swipers.js
@@ -1,8 +1,8 @@
 // Brands Initialized
-var slidesPerView = 4;
+var brandsSlidesPerView = 4;
 
 new Swiper('#swiper-1', {
-	slidesPerView: slidesPerView,
+	slidesPerView: brandsSlidesPerView,
 	speed: 400,
 	spaceBetween: 16,
 	grabCursor: true,
@@ -12,12 +12,42 @@ new Swiper('#swiper-1', {
 	}
 });
 new Swiper('#swiper-2', {
-	slidesPerView: slidesPerView,
+	slidesPerView: brandsSlidesPerView,
 	speed: 400,
 	spaceBetween: 16,
 	grabCursor: true,
 	navigation: {
 		nextEl: '#swiper-button-next-2',
 		prevEl: '#swiper-button-prev-2'
+	}
+});
+
+// Featured Products Initialized
+
+new Swiper('#fi__swiper', {
+	slidesPerView: 1.1,
+	speed: 400,
+	spaceBetween: 16,
+	grabCursor: true,
+	slidesOffsetBefore: 0,
+	slidesOffsetAfter: 0,
+	navigation: {
+		nextEl: '#fi__swiper-button-next',
+		prevEl: '#fi__swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination'
+	},
+	breakpoints: {
+		600: {
+			slidesPerView: 1.7,
+			slidesOffsetBefore: 50,
+			slidesOffsetAfter: 50
+		},
+		900: {
+			slidesPerView: 2.8,
+			slidesOffsetBefore: 50,
+			slidesOffsetAfter: 50
+		}
 	}
 });
